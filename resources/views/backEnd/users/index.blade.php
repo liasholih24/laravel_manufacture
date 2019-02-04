4936a407737feeebd1772c6fa919980e9eb35dd5
Lia Siti Sholihah <liasiti@1s-MacBook-Air.local>
2019-02-04 12:13:07 +0700

    user index

--- resources/views/backEnd/users/index.blade.php
+++ resources/views/backEnd/users/index.blade.php
@@ -21,14 +21,19 @@ Modul Manajamen Pengguna
 						.ibox.float-e-margins{ margin: 0px 2px !important}
 			 </style>
             <div class="row ibox-title">
+            <ol class="breadcrumb col-sm-6 col-xs-12" style="font-size: 13px; padding-top: 6px; ">
+              <li>
+                  Select Role 
+              </li>
+            </ol>
                 @if (Sentinel::getUser()->hasAccess(['user.create']))
                 <a href="{{ url('user/create') }}"><button class="btn btn-sm btn-outline btn-info pull-right" style="margin-right: 10px">
                     <i class="fa fa-plus-circle" style="margin-right: 5px"></i> Add New User
                 </button>
                 </a>
                 @endif
-              <div class="col-sm-2" style="position:absolute;">
-                <select class="select2_demo_1"  style="margin-right: 10px;" onchange="if (this.value) window.location.href=this.value">
+              <div class="col-sm-2" style="position:absolute;margin-left: 60px;">
+                <select class="select2_demo_1"  style="" onchange="if (this.value) window.location.href=this.value">
                 @foreach($roles as $role)
                   <option value="{{ url('user') }}">Select Roles</option>
                   <option value="{{ url('user/'.$role->id.'/role') }}" <?php if ($role->id == $id) echo ' selected="selected"'; ?>>{{$role->name}}</option>
