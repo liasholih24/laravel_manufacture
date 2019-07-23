4745ef5319f390ae03f9208173abf3eeb6418bb2
Lia Siti Sholihah <liasiti@1s-MacBook-Air.local>
2019-07-23 15:42:05 +0700

    pengajuan index

--- resources/views/backEnd/pengajuan/index.blade.php
+++ resources/views/backEnd/pengajuan/index.blade.php
@@ -57,9 +57,8 @@
                                         <td>{{ $r->desc }}</td>
                                         <td width="35%">
                                         <ul>
-                                            @foreach ($r->items as $item)
-                                                <li>{{ empty($item->item->name)? '' : $item->item->name.' ,Qty: '.$item->qty.' '.$item->satuan->name   }} </li>
-                                            @endforeach
+
+                                           {{$r->items}}
                                         </ul>
                                         </td>
                                         <td>{{ empty($r->createdby->first_name) ? "" : $r->createdby->first_name }} {{ empty($r->createdby->last_name) ? "" : $r->createdby->last_name }}</td>
